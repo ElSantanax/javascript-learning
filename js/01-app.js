855c5f25abda25b2a2279f09b4cf6c61c5007eb5
ElSantanax <josesantanaserrano130598@gmail.com>
2025-08-26 12:34:55 -0500

    Scope

---
+++ js/01-app.js
@@ -0,0 +1,14 @@
+// Scope
+// Determina dónde se pueden acceder y modificar las variables. Hay varios tipos de alcance en JavaScript:
+// Alcance Global
+// Alcance de Función
+// Alcance de Bloque
+
+const cliente = 'José';
+
+function mostrarCliente() {
+    console.log(cliente);
+}
+
+mostrarCliente();
+
